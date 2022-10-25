358fdab0dfb9788e98ad4dd70ba0fb0674eb3b95
GitMaiHub <h.maibcarlton.i@gmail.com>
2022-10-25 13:52:50 -0600

    Change the defaultCity.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <SearchEngine defaultCity="Lapland" />
+        <SearchEngine defaultCity="Bruges" />
       </div>
       <footer>
         This project is{" "}
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,18 +1,30 @@
 import React from "react";
 
 export default function SearchEngine(props) {
-    return (
-        <div className="SearchEngine">
-            <form>
-                <div className="row">
-                    <div className="col-10">
-                        <input type="search" placeholder="Enter a city..." autoFocus={true} className="form-control" />
-                    </div>
-                    <div className="col-2">
-                        <input type="submit" value="🔍" className="btn btn-primary" />
-                    </div>
-                </div>
-            </form>
-        </div>
-    )
+
+  
+
+   return (
+     <div className="SearchEngine">
+       <form>
+         <div className="row">
+           <div className="col-10">
+             <input
+               type="search"
+               placeholder="Enter a city..."
+               autoFocus={true}
+               className="form-control"
+           
+             />
+           </div>
+           <div className="col-2">
+             <input type="submit" value="Search" className="btn btn-success" />
+           </div>
+         </div>
+       </form>
+     </div>
+   );
+
+
 }
+ 
